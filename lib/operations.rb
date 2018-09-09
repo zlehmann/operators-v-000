bdd13e3d11972d59d517d9d4ae78cea00b20f090
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-09 02:09:08 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+  if speed > 40 && speed < 60
+    return false
+  else
+    return true
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  speed > 40 && speed < 60 ? out=false : out=true
+  return out
 end
-	
-
-
